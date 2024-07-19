47e8636933c9cb600da699db4e0948bd53155d59
Андрей Полушин <pandser@yandex.ru>
2024-07-19 21:19:24 +0300

    update models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.sessions.models import Session
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class CityInSession(models.Model):
+    session_id = models.ForeignKey(
+        Session,
+        on_delete=models.CASCADE,
+        related_name='session_id',
+    )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        related_name='city',
+    )
